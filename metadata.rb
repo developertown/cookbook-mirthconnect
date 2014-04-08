eae1e05ac74d439f6482213648c5fea10e185c7f
Jason Vasquez <jason.vasquez@developertown.com>
2014-04-08 07:53:49 -0400

    Updating metadata to include name and proper maintainer info

--- metadata.rb
+++ metadata.rb
@@ -1,5 +1,6 @@
-maintainer       "Diagnotes, Inc."
-maintainer_email "admin@diagnotes.net"
+name             "MirthConnect"
+maintainer       "DeveloperTown, LLC"
+maintainer_email "admin@developertown.com"
 license          "Apache 2.0"
 description      "Installs/Configures Mirth Connect"
 
